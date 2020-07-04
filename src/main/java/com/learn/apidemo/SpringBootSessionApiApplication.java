6f01d0267d174ee8fc33e3190929544153fca7fa
laxman <ch.laxmanmurthy@gmail.com>
2020-07-04 23:21:40 +0530

    Initial commit

---
+++ src/main/java/com/learn/apidemo/SpringBootSessionApiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.apidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/apidemo/SpringBootSessionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSessionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
